032bc2f9110ed03afff46ff33ff2008c09d7f270
Buddhika Athukorala <buddhikaathu@users.noreply.github.com>
2023-12-06 15:58:38 +0530

    Add files via upload

---
+++ storage/framework/views/81f5101eff274f151be50a90329831a9b9bd114d.php
@@ -0,0 +1,16 @@
+
+<?php $__env->startSection('title'); ?>
+    My To Do App
+<?php $__env->stopSection(); ?>
+<?php $__env->startSection('content'); ?>
+<div class="row mt-3">
+    <div class="col-12 align-self-center">
+        <ul class="list-group">
+            <?php $__currentLoopData = $todo; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                <li class="list-group-item"><a href="details/<?php echo e($item->id); ?>" style="color: cornflowerblue"><?php echo e($item->name); ?></a></li>
+            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+        </ul>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\todoapp\resources\views/index.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/b06a976c82dd8248f185227e946efc3e34c3a4f6.php
@@ -0,0 +1,29 @@
+
+<?php $__env->startSection('title'); ?>
+    Edit
+<?php $__env->stopSection(); ?>
+<?php $__env->startSection('content'); ?>
+
+<form action="../update" method="POST" class="mt-4 p-4">
+    <?php echo csrf_field(); ?>
+    <?php if(session()->has('success')): ?>
+    <div class="alert alert-success">
+        <?php echo e(session->get('success')); ?>
+
+    </div>
+    <?php endif; ?>
+    <div class="form-group m-3">
+        <label for="name">Todo Name</label>
+        <input type="text" class="form-control" name="name" value="<?php echo e($todo->name); ?>">
+    </div>
+    <div class="form-group m-3">
+        <label for="description">Todo Description</label>
+        <textarea class="form-control" rows="3" name="description"><?php echo e($todo->description); ?></textarea>
+    </div>
+    <div class="form-group m-3">
+        <input type="submit" class="btn btn-primary float-end" value="Update">
+        <input type="hidden" name="id" value="<?php echo e($todo->id); ?>">
+    </div>
+</form>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\todoapp\resources\views/edit.blade.php ENDPATH**/ ?>
